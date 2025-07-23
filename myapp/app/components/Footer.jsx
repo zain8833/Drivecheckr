5ada28e2ab24a0c3b85a8d794ac99dcfa6ec8cc9
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-07-23 20:22:46 +0500

    sad

---
+++ myapp/app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-800 text-white text-center py-4">
+      <p>© {new Date().getFullYear()} Vehicle Inspection. All rights reserved.</p>
+    </footer>
+  );
+}
