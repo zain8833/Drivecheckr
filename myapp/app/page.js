c15203798bfbb704d42d8ed3c78de02a251a796d
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-07-25 19:50:53 +0500

    sds

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
