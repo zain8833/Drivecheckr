042cfc0e3c0f5a14c6f0773159c9d7971a9e6660
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-08-04 21:44:51 +0500

    ass

--- myapp/next.config.mjs
+++ myapp/next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable @next/next/no-img-element */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    images: {
+    unoptimized: true, // IMPORTANT for static export
+  },
 };
 
 export default nextConfig;
