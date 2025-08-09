8f238b47234c2354ae6db7a26d866f574b9e357f
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-08-09 21:48:55 +0500

    kmkjvm

---
+++ myapp/app/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div>
+        <section id="contact" className="py-20 bg-white text-center px-4">
+                <h2 className="text-3xl font-bold mb-6">📬 Contact Us</h2>
+                <p className="text-gray-600 mb-6">We're here to help! Fill out the form below for any queries or appointment requests.</p>
+                <Form />
+              </section>
+    </div>
+  )
+}
+
+export default Content
